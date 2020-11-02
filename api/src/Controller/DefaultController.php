342b4068cde82a4cf416716a4329c8dc06b81082
Ruben van der Linde <ruben@conduction.nl>
2020-11-02 16:24:24 +0100

    Controller clean up and first texts

---
+++ api/src/Controller/DefaultController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * The API test handles all requests that should be redirected to a component for the benefit of AJAX Calls.
+ *
+ * Class ApiController
+ *
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Template
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        $variables = [];
+
+        return  $variables;
+    }
+
+    /**
+     * @Template
+     * @Route("/challanges")
+     */
+    public function challangesAction()
+    {
+        $variables = [];
+
+        return  $variables;
+    }
+
+}
