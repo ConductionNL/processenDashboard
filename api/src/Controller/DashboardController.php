28f4fcd698262a5dc57559f1af7f03e8f58ad18b
Ruben van der Linde <ruben@conduction.nl>
2020-11-03 08:24:05 +0100

    First controller code

--- api/src/Controller/DashboardController.php
+++ api/src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,7 +21,7 @@ class DashboardController extends AbstractController
      * @Template
      * @Route("/")
      */
-    public function indexAction()
+    public function indexAction(CommonGroundService $commonGroundService, Request $request)
     {
         $variables = [];
 
@@ -31,44 +32,184 @@ class DashboardController extends AbstractController
      * @Template
      * @Route("/clusters")
      */
-    public function clustersAction()
+    public function clustersAction(CommonGroundService $commonGroundService, Request $request)
     {
         $variables = [];
 
-        return  $variables;
+        // On an index route we might want to filter based on user input
+        $variables['query'] = array_merge($request->query->all(), $variables['post'] = $request->request->all());
+
+        // Get resource tutorials (known as cources component side)
+        $variables['clusters'] = $commonGroundService->getResource(['component' => 'evc', 'type' => 'clusters'], $variables['query'])['hydra:member'];
+
+        return $variables;
+    }
+
+    /**
+     * @Template
+     * @Route("/clusters/{id}")
+     */
+    public function clusterAction(CommonGroundService $commonGroundService, Request $request, $id)
+    {
+        $variables = [];
+
+        if ($id != 'new') {
+            // Get resource challenges (known as tender component side)
+            $variables['cluster'] = $commonGroundService->getResource(['component' => 'evc', 'type' => 'enviroments', 'id'=>$id]);
+        } else {
+            $variables['cluster'] = [];
+        }
+
+        // Lets see if there is a post to procces
+        if ($request->isMethod('POST')) {
+            $resource = $request->request->all();
+
+            // Add the post data to the already aquired resource data
+            $resource = array_merge($variables['resource'], $resource);
+
+            // Update to the commonground component
+            $variables['cluster'] = $commonGroundService->saveResource($resource, ['component' => 'evc', 'type' => 'enviroments', 'id' => $id]);
+        }
+
+        return $variables;
     }
 
     /**
      * @Template
      * @Route("/installations")
      */
-    public function installationsAction()
+    public function installationsAction(CommonGroundService $commonGroundService, Request $request)
     {
         $variables = [];
 
-        return  $variables;
+        // On an index route we might want to filter based on user input
+        $variables['query'] = array_merge($request->query->all(), $variables['post'] = $request->request->all());
+
+        // Get resource tutorials (known as cources component side)
+        $variables['installations'] = $commonGroundService->getResource(['component' => 'evc', 'type' => 'installations'], $variables['query'])['hydra:member'];
+
+        return $variables;
+    }
+
+    /**
+     * @Template
+     * @Route("/installations/{id}")
+     */
+    public function installationAction(CommonGroundService $commonGroundService, Request $request, $id)
+    {
+        $variables = [];
+
+        if ($id != 'new') {
+            // Get resource challenges (known as tender component side)
+            $variables['installation'] = $commonGroundService->getResource(['component' => 'evc', 'type' => 'installations', 'id'=>$id]);
+        } else {
+            $variables['installation'] = [];
+        }
+
+        // Lets see if there is a post to procces
+        if ($request->isMethod('POST')) {
+            $resource = $request->request->all();
+
+            // Add the post data to the already aquired resource data
+            $resource = array_merge($variables['resource'], $resource);
+
+            // Update to the commonground component
+            $variables['installation'] = $commonGroundService->saveResource($resource, ['component' => 'evc', 'type' => 'installations', 'id' => $id]);
+        }
+
+        return $variables;
     }
 
     /**
      * @Template
      * @Route("/components")
      */
-    public function componentsAction()
+    public function componentsAction(CommonGroundService $commonGroundService, Request $request)
     {
         $variables = [];
 
-        return  $variables;
+        // On an index route we might want to filter based on user input
+        $variables['query'] = array_merge($request->query->all(), $variables['post'] = $request->request->all());
+
+        // Get resource tutorials (known as cources component side)
+        $variables['components'] = $commonGroundService->getResource(['component' => 'evc', 'type' => 'components'], $variables['query'])['hydra:member'];
+
+        return $variables;
+    }
+
+    /**
+     * @Template
+     * @Route("/components/{id}")
+     */
+    public function componentAction(CommonGroundService $commonGroundService, Request $request, $id)
+    {
+        $variables = [];
+
+        if ($id != 'new') {
+            // Get resource challenges (known as tender component side)
+            $variables['component'] = $commonGroundService->getResource(['component' => 'evc', 'type' => 'components', 'id'=>$id]);
+        } else {
+            $variables['component'] = [];
+        }
+
+        // Lets see if there is a post to procces
+        if ($request->isMethod('POST')) {
+            $resource = $request->request->all();
+
+            // Add the post data to the already aquired resource data
+            $resource = array_merge($variables['resource'], $resource);
+
+            // Update to the commonground component
+            $variables['component'] = $commonGroundService->saveResource($resource, ['component' => 'evc', 'type' => 'components', 'id' => $id]);
+        }
+
+        return $variables;
     }
 
     /**
      * @Template
      * @Route("/databases")
      */
-    public function databasesAction()
+    public function databasesAction(CommonGroundService $commonGroundService, Request $request)
     {
         $variables = [];
 
-        return  $variables;
+        // On an index route we might want to filter based on user input
+        $variables['query'] = array_merge($request->query->all(), $variables['post'] = $request->request->all());
+
+        // Get resource tutorials (known as cources component side)
+        $variables['databases'] = $commonGroundService->getResource(['component' => 'evc', 'type' => 'databases'], $variables['query'])['hydra:member'];
+
+        return $variables;
+    }
+
+    /**
+     * @Template
+     * @Route("/databases/{id}")
+     */
+    public function databaseAction(CommonGroundService $commonGroundService, Request $request, $id)
+    {
+        $variables = [];
+
+        if ($id != 'new') {
+            // Get resource challenges (known as tender component side)
+            $variables['database'] = $commonGroundService->getResource(['component' => 'evc', 'type' => 'databases', 'id'=>$id]);
+        } else {
+            $variables['database'] = [];
+        }
+
+        // Lets see if there is a post to procces
+        if ($request->isMethod('POST')) {
+            $resource = $request->request->all();
+
+            // Add the post data to the already aquired resource data
+            $resource = array_merge($variables['resource'], $resource);
+
+            // Update to the commonground component
+            $variables['database'] = $commonGroundService->saveResource($resource, ['component' => 'evc', 'type' => 'databases', 'id' => $id]);
+        }
+
+        return $variables;
     }
 
     /**
@@ -79,6 +220,56 @@ class DashboardController extends AbstractController
     {
         $variables = [];
 
+        // On an index route we might want to filter based on user input
+        $variables['query'] = array_merge($request->query->all(), $variables['post'] = $request->request->all());
+
+        // Get resource tutorials (known as cources component side)
+        $variables['bc'] = $commonGroundService->getResource(['component' => 'evc', 'type' => 'invoices'], $variables['query'])['hydra:member'];
+
+        return $variables;
+    }
+
+    /**
+     * @Template
+     * @Route("/invoices/{id}")
+     */
+    public function invoiceAction(CommonGroundService $commonGroundService, Request $request, $id)
+    {
+        $variables = [];
+
+        $variables['invoice'] = $commonGroundService->getResource(['component' => 'bc', 'type' => 'invoices', 'id'=>$id]);
+
         return  $variables;
+    }
+
+    /* geen slecht idee om organization stuff naar een eigne controller te verplaatsen */
+
+    /**
+     * @Template
+     * @Route("/organization")
+     */
+    public function organizationAction()
+    {
+        $variables = [];
+
+        if ($id != 'new') {
+            // Get resource challenges (known as tender component side)
+            $variables['organization'] = $commonGroundService->getResource(['component' => 'wrc', 'type' => 'organizations', 'id'=>$id]);
+        } else {
+            $variables['organization'] = [];
+        }
+
+        // Lets see if there is a post to procces
+        if ($request->isMethod('POST')) {
+            $resource = $request->request->all();
+
+            // Add the post data to the already aquired resource data
+            $resource = array_merge($variables['resource'], $resource);
+
+            // Update to the commonground component
+            $variables['organization'] = $commonGroundService->saveResource($resource, ['component' => 'wrc', 'type' => 'organizations', 'id' => $id]);
+        }
+
+        return $variables;
     }
 }
